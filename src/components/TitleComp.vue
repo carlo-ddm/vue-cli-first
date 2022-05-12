d9f0de6c9805c2f4ab42c90ae0cf7e1bcedf3f7e
carlo-ddm <carloduminuco90@gmail.com>
2022-05-12 17:29:55 +0200

    consegna + Bonus

---
+++ src/components/TitleComp.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>La mia prima app con Vue CLI!</h1>
+  <img src="../assets/logo.png" alt="occhiali">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TitleComp',
+}
+</script>
+
+<style>
+
+</style>
